07622b8ea17ee390e71ea331cf02590024f2850a
Alex Bokov <bokov@uthscsa.edu>
2023-12-14 12:43:36 -0600

    bejewered.R; core features; compaction, scoring, updating swapping

--- bejewered.R
+++ bejewered.R
@@ -1,20 +1,20 @@
 # functions ----
 
 createBJW <- function(nr=sample(3:8,1),nc=sample(3:8,1)
-                      ,colors=sample(palette(),nr*nc,rep=T)){
+                      ,colors=sample(1:4,nr*nc,rep=T)){
   out <- new.env();
   out$data <- matrix(colors,nr,nc);
   out$selected1 <- c();
+  out$score <- data.frame(length=factor(),type=factor(),Freq=integer(),rc=character());
   class(out) <- c('bjw','list','environment');
-  #out$score <- list();
   out;
 }
 
 parse_bjw_id <- function(identifier,retval=c('both','rc','id')
-                         ,prefix=getOption('bjw.cell.prefix','bjwtd_')
-                         ,bjw,rcmax){
+                       ,prefix=getOption('bjw.cell.prefix','bjwtd_')
+                         ,bjw=NA,rcmax){
   out <- list();
-  if(missing(rcmax)) rcmax <- if(missing(bjw)) c(8,8) else dim(bjw$data);
+  if(missing(rcmax)) rcmax <- if(!is(bjw,'bjw')) c(8,8) else dim(bjw$data);
   if(is.numeric(identifier) && length(identifier) == 2){
     out<-list(rc=identifier,id=paste0(prefix,row,'_',col));
   } else if(is.character(identifier) && length(identifier) == 1 &&
@@ -25,18 +25,18 @@ parse_bjw_id <- function(identifier,retval=c('both','rc','id')
   } else stop('The selected2 argument to the select.bjw() function must be either a character of length 1 or numeric of length 2, specifying either the ID or the coordinates of the cell you wish to select');
   if(!all(between(out$rc,1,rcmax))){
     msg <- c('parse_bjw_id as given an identifier (',out$id,' or ',paste0(out$rc,collapse=', '),') which might map to a cell that does not exist, proceed with caution');
-    if(missing(bjw)) warning(msg) else stop(msg);
+    if(!is(bjw,'bjw')) warning(msg) else stop(msg);
   };
   switch(match.arg(retval),rc=out$rc,id=out$id,out);
 };
 
-isadjacent_bjw_id <- function(ident2,ident1=bjw$selected,bjw){
+isadjacent_bjw_id <- function(ident2,ident1=bjw$selected1,bjw){
   delta<-abs(parse_bjw_id(ident1,'rc')-parse_bjw_id(ident2,'rc'));
   all(0:1 %in% delta);
   };
 
 select.bjw <- function(bjw,selected2,selected1=bjw$selected1){
-  id <- parse_bjw_id(bjw,selected2,'id');
+  id <- parse_bjw_id(selected2,'id',bjw=bjw);
   # if there is no bjw$selected1 that means this is the first half of a paired
   # selection, so update accordingly and return
   if(length(selected1)==0){bjw$selected1 <-id; return(bjw)};
@@ -45,9 +45,10 @@ select.bjw <- function(bjw,selected2,selected1=bjw$selected1){
   if(selected1 == id) {bjw$selected1 <- c(); return(bjw)};
   # if the selection is not adjacent, that's a signal to select the new cell
   # instead as the first half of the selection
-  if(!isadjacent_bjw_id(bjw,id)){bjw$selected1 <-id; return(bjw)};
+  if(!isadjacent_bjw_id(id,bjw=bjw)){bjw$selected1 <-id; return(bjw)};
   # only if none of the above conditions are met, can we swap
-  # swap.bjw(bjw,selected2, selected1);
+  # TODO: only permit swaps that would result in scoring!
+  swap.bjw(selected2, bjw=bjw); bjw$selected1 <- c();
   bjw;
 };
 
@@ -75,41 +76,76 @@ swap.bjw <- function(bjw,ident2,ident1=bjw$selected1){
 
 # TODO:
 compact.bjw <- function(bjw){
-  # for each column, move all the non-NA values to the bottom, and fill in the top with NAs
+  bjw$data <- apply(bjw$data,2,function(xx) c(rep_len(NA,sum(is.na(xx))),na.omit(xx)));
   bjw;
 }
 
-# TODO:
-match.bjw <- function(bjw){
+row_bjw_analyze <- function(xx){
+  scoretemplate <- data.frame(length=factor(),type=factor(),Freq=integer());
+  rl <- rle(xx);
+  idx <- with(rl,which(lengths>=3 & !is.na(values)));
+  score <- with(rl,table(length=lengths[idx],type=values[idx]) %>%
+                  as.data.frame() %>% rbind(scoretemplate,.));
+  rl$values[idx] <- NA;
+  return(list(score=score,resultrow=inverse.rle(rl)));
+}
+
+match.bjw <- function(bjw,update_bjw=T){
   # for each row, look for runs of 3, 4, 5, etc.
+  result_x <- apply(bjw$data,1,row_bjw_analyze);
+  result_y <- apply(bjw$data,2,row_bjw_analyze);
   # update the counts of individual color and score combos separately (e.g. red triples, red quadruples, etc. then move on to green)
+  score_x <-lapply(result_x,function(xx) xx$score) %>% do.call(bind_rows,.) %>% mutate(rc=if(n()>0) 'r' else character());
+  score_y <-lapply(result_y,function(xx) xx$score) %>% do.call(bind_rows,.) %>% mutate(rc=if(n()>0) 'r' else character());
+  score_xy <- rbind(bjw$score,score_x,score_y) %>% group_by(rc,length,type) %>% summarise(Freq=sum(Freq));
   # after recording the score for each, set those values to NA
-  bjw;
+  nas_x<-lapply(result_x,function(xx) xx$resultrow) %>% do.call(rbind,.);
+  nas_y<-lapply(result_y,function(xx) xx$resultrow) %>% do.call(rbind,.) %>% t;
+  nas_xy <- pmax(nas_x,nas_y);
+  if(update_bjw){
+    # update bjw
+    bjw$data <- nas_xy;
+    bjw$score <- score_xy;
+    bjw;
+  } else list(score=score,data=data);
 }
 
-# TODO:
-refill.bjw <- function(bjw){
-  # for each column identify the maximum series of NAs that form an uninterrupted path to the top edge
+refill.bjw <- function(bjw,refillwith=unique(na.omit(c(bjw$data)))){
   # randomly fill with colors
+  nas <- is.na(bjw$data);
+  bjw$data[nas] <- sample(refillwith,sum(nas),rep=T);
   bjw;
 }
 
-# TODO:
-innerUpdate.bjw <- function(bjw){
+plot.bjw <- function(x,...){
+  print(x$score);
+  print(datatable(x$data,colnames = '',options=list(paging=F,searching=F)));
+}
+
+totalscore.bjw <- function(bjw){with(bjw$score,sum(as.numeric(as.character(length))*Freq))};
+
+innerUpdate.bjw <- function(bjw,verbose=0){
   converged <- F;
+  if(verbose) plot.bjw(bjw);
   while(!converged){
     previousstate <- bjw$data;
     compact.bjw(bjw);
+    if(verbose) plot.bjw(bjw);
     match.bjw(bjw);
+    if(verbose) plot.bjw(bjw);
     converged <- identical(bjw$data,previousstate);
   }
 };
 
-outerUpdate.bjw <- function(bjw){
+outerUpdate.bjw <- function(bjw,verbose=0){
   converged <- F;
+  if(verbose) plot.bjw(bjw);
   while(!converged){
+    previousstate <- bjw$data;
     refill.bjw(bjw);
-    innerUpdate.bjw(bjw);
+    if(verbose) plot.bjw(bjw);
+    innerUpdate.bjw(bjw,max(verbose-1,0));
+    converged <- identical(bjw$data,previousstate);
   }
 }
 
